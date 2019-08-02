bc449b805954d2e9fb0c46f12adc6731b1313f04
Thanigaivasan <thanigaivasan27@gmail.com>
2019-08-02 13:43:28 +0530

    initial project version

---
+++ objects.js
@@ -0,0 +1,27 @@
+//objects excercise
+//Question in bit.ly/js102-exercises
+
+var animal={};
+animal.username="Lion";
+console.log(animal.username);
+
+animal["tagline"]="I am the King of the Jungle";
+console.log(animal["tagline"]);
+
+var noises=[];
+animal["noises"]=noises;
+console.log(animal);
+
+var count=0;
+for(var key in animal){
+    count++;
+    if(key === "username")
+    {
+        console.log("Hi my name is "+animal[key]);
+    }
+    else if(key === "tagline")
+    {
+        console.log("I like to say "+animal[key]);
+    }
+}
+
