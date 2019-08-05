1017c7f0e373ba1fccd19e7b6addfec407c39386
Thanigaivasan <thanigaivasan27@gmail.com>
2019-08-05 13:30:17 +0530

    Finished the function exercise

--- functions.js
+++ functions.js
@@ -30,17 +30,46 @@ var AnimalCreator=function(username,species,tagline,noises){
     };   
 }
 
-var sheep = AnimalCreator('Cloud', 'sheep', 'You can count on me!', ['baahhh', 'arrgg', 'chewchewchew']);
-console.log(sheep);
+var sheep = AnimalCreator('Cloud', 'Sheep', 'You can count on me!', ['baahhh', 'arrgg', 'chewchewchew']);
 
-var Lion = AnimalCreator('Cloud', 'sheep', 'You can count on me!', ['baahhh', 'arrgg', 'chewchewchew']);
-console.log(Lion);
+var lion = AnimalCreator('Simba', 'Lion', 'King of the jungle', ['roar', 'growl', 'pur']);
+
+var duck = AnimalCreator('Daffy', 'Duck', 'Yippeee', ['squeaks', 'cock-a-doodle-do', 'quack']);
 
 var addFriend=function(animal,frnd){
-    animal.friends.push(frnd.species);
+    animal.friends.push(frnd.username);
 }
 
-addFriend(sheep,Lion);
-console.log(sheep);
+addFriend(sheep,lion);
+addFriend(sheep,duck);
+addFriend(duck,lion);
+addFriend(lion,sheep);
+addFriend(lion,duck);
+addFriend(duck,sheep);
 
-//yet to complete
+
+var myfarm=[];
+myfarm.push(sheep);
+myfarm.push(lion);
+myfarm.push(duck);
+
+var addMatchesArray = function(myfarm){
+    for(i=0;i<myfarm.length;i++)
+    {
+       myfarm[i].matches=[];
+    } 
+}
+
+addMatchesArray(myfarm);
+
+var giveMatches=function(myfarm){
+    for(i=0;i<myfarm.length;i++)
+    {
+        myfarm[i].matches[0]=myfarm[i].friends[0];
+    }
+}
+
+giveMatches(myfarm);
+console.log(myfarm[1]);
+
+
